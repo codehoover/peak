68c3727d7b35e4bd037e76f4002304d468569ba8
Esau R <mumbonumber7@gmail.com>
2024-03-08 14:35:04 -0600

    bugfix navigation to mobilenav

---
+++ src/components/MobileNav/HamburgerMenu.jsx
@@ -0,0 +1,10 @@
+export default function HamBurgerMenu(){
+
+    return(
+        <div id="hamburger-menu">
+            <div id="burger"/>
+            <div id="burger"/>
+            <div id="burger"/>
+        </div>
+    )
+}
---
+++ src/components/MobileNav/MobileNav.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import HamBurgerMenu from "./HamburgerMenu";
+import { House, Info, ContactSupport, Login, PersonAdd } from "@mui/icons-material";
+
+export default function MobileNav(){
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () =>{
+        setIsOpen(!isOpen);
+
+    }
+
+    return(
+        <div id="hamburger-container">
+            <div id="hamburger" onClick={toggleMenu}>
+                <HamBurgerMenu/>
+            </div>
+            {isOpen ? <div id="mobile-navigation">
+                <ul>
+                    <Link to="/" id="mobile-nav-link"> <House/>Home</Link>
+                    <Link to="/About" id="mobile-nav-link"> <Info/>About</Link>
+                    <Link to="/Contact" id="mobile-nav-link"> <ContactSupport/>Contact Us</Link>
+                    <Link to="/Login" id="mobile-nav-link"> <Login/>Login</Link>
+                    <Link to="/SignUp" id="mobile-nav-link"> <PersonAdd/> Sign Up</Link>
+                </ul>
+            </div>: null}
+
+        </div>
+    )
+}
