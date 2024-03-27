e9cc34c3abb819d328302a0699f811003546f26a
Esau R <mumbonumber7@gmail.com>
2024-03-27 14:25:27 -0500

    pushing weight changes

--- src/components/WeightTracker.jsx
+++ src/components/WeightTracker.jsx
@@ -1,23 +1,41 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Chart as Chartjs } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 
 export default function WeightTracker(){
-
-    const [weightinput, setWeightInput] = useState([{weight:""}])
-
-    const handleChange = (event,index) => {
-        let { name,value } = event.target; //name,value = key,value
-        let onChangeValue = [...weightinput]; //spread inputs
-        onChangeValue[index][name] = value; //set value equal to name
-    }
     return(
         <div>
             <h1> Weight Tracker</h1>
             <p>
                 Input your daily weight for the week below.
             </p>
+
             <div id="weight-input">
+                
+                
+                <Line
+                
+                data={{
+                    labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                    datasets: [
+                        {
+                            label: "Weight",
+                            data: [135,140,136,145,140,140,145],
+                        },
+                    ]
+
+                }}
+                
+                
+                />
 
             </div>
+
+            <div>
+                <input/>
+                <button>button</button>
+            </div>
+            
         </div>
     )
 }
